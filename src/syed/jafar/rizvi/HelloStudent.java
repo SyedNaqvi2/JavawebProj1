09094290282973c6accb68a871e2ff831335045e
jaferrizvi <jafershah34@gmail.com>
2021-02-17 07:30:57 +0500

    Update HelloStudent.java

--- src/syed/jafar/rizvi/HelloStudent.java
+++ src/syed/jafar/rizvi/HelloStudent.java
@@ -2,9 +2,13 @@ package syed.jafar.rizvi;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.Enumeration;
+import java.util.Map;
+import java.util.Set;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -45,16 +49,69 @@ public class HelloStudent extends HttpServlet {
         String Score = request.getParameter("StudentScore");
         String Attendance = request.getParameter("StudentAttendance");
         String Grade = request.getParameter("StudentGrade");
+        
+        Map<String,String[]> map = request.getParameterMap();
+        Set<String> keys = map.keySet();
+        
+          
         out.println("Student Name: " + name + "<br>");
         out.println("Student Score: " + Score + "<br>");
         out.println("Student Attendance: " + Attendance + "<br>");
         out.println("Student Grade: " + Grade + "<br>");
+        out.println("all the keys are: " + keys.toString() + "<br>");
+        
+        for (String parameterName :keys) {
+            String[] values = map.get(parameterName);
+            if (values != null && values.length > 0) {
+            	out.println(parameterName + ": " + values[0] + "<br>");
+              
+            } 
+          }
+        
+        out.println("REQUEST INFO FROM REQUEST OBJECT" + "<br>");
+        out.println("Method: " + request.getMethod() + "<br>");
+        out.println("Request URI: " + request.getRequestURI() + "<br>");
+        out.println("Protocol: " + request.getProtocol() + "<br>");
+        out.println("PathInfo: " + request.getPathInfo() + "<br>");
+        out.println("Remote Address: " + request.getRemoteAddr() + "<br>");
+        out.println("REQUEST HEADER INFO" + "<br>");
+        Enumeration e = request.getHeaderNames();
+        while (e.hasMoreElements()) {
+            String nameH = (String)e.nextElement();
+            String valueH = request.getHeader(nameH);
+            out.println(nameH + " = " + valueH + "<br>");
+        }
+        
+        // set a cookie
+
+        String cname = "userName";
+        String cvalue ="sbaqirnaqvi";
+        String passw ="password";
+        String passwval = "baqir1234567";
+       
+         Cookie c1 = new Cookie(cname, cvalue);
+         Cookie c2 = new Cookie(passw, passwval);
+         response.addCookie(c1);
+         response.addCookie(c2);
+        
+    
+     // print out cookies
+
+        Cookie[] cookies = request.getCookies();
+        for (int i = 0; i < cookies.length; i++) {
+            Cookie c = cookies[i];
+            String name2 = c.getName();
+            String value2 = c.getValue();
+            out.println(name2 + " = " + value2  + "<br>");
+        }
+
+        
         out.println("<a href=\"http://localhost:8080/HelloWorldJafar/StudentRegForm.html\">Student Registration Form</a>");
         //out.println("<a href="+"http://localhost:8080/HelloWorldJafar/StudentRegForm.html"+"> Student Registration Form" + "</a>");
         out.println("</body>");       
         out.println("</html>");
         //response.sendRedirect("http://www.google.com");
-        String studentRecord = name + " " + Score + " " + Grade + " " + Attendance + "\n";  
+        String studentRecord = "\n" + name + " " + Score + " " + Grade + " " + Attendance;  
         StudentFileData.Writefile(StudentFileData.FILE_PATH , StudentFileData.FILE_NAME, studentRecord);
 	}
 
